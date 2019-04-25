b2923c2de2a87732ea469c29f3e4d59bf87bd2ab
qwe8365879 <jjj.jjjiang@gmail.com>
2019-04-25 17:47:04 +1000

    basic function done

--- src/App.js
+++ src/App.js
@@ -22,33 +22,42 @@ class App extends Component {
         });
     };
 
+    calculateHours = input => {
+        let timeTester = /(\d?\d:\d\d(\s[AaPp][Mm]\s)?\sTO\s\d?\d:\d\d(\s[AaPp][Mm]\s)?)/g;
+        var result = input.split(timeTester);
+        console.dir(result);
+        result.map(function(v, k){
+            if(typeof v =="string" && v.match(timeTester)){
+                var times = v.split(" TO ");
+                var start = times[0].trim();
+                var end = times[1].trim();
+                if(start.match(/[AaPp][Mm]/)){
+                    start = moment(start, 'hh:mm a');
+                }else{
+                    start = moment(start, 'hh:mm')
+                }
+                if(end.match(/[AaPp][Mm]/)){
+                    end = moment(end, 'hh:mm a');
+                }else{
+                    end = moment(end, 'hh:mm')
+                }
+                var hours = moment.duration(end.diff(start)).asHours();
+                result[k] = hours;
+            }else if(typeof v =="string" && v.match(/[AaPp][Mm]/)){
+                result[k] = "";
+            }
+        });
+        result = result.join("");
+        console.dir(result);
+        return result;
+    };
+
     handleEquals = () => {
         var input = this.state.input;
-        if(input.match(/\d?\d:\d\d(\sAM\s)?\sTO\s\d?\d:\d\d(\sPM\s)?/)){
-            var times = this.state.input.split(" TO ");
-            var start = times[0].trim();
-            var end = times[1].trim();
-            if(start.match(/[AaPp][Mm]/)){
-                start = moment(start, 'hh:mm a');
-            }else{
-                start = moment(start, 'hh:mm')
-            }
-            if(end.match(/[AaPp][Mm]/)){
-                end = moment(end, 'hh:mm a');
-            }else{
-                end = moment(end, 'hh:mm')
-            }
-            var hours = moment.duration(end.diff(start)).asHours();
-            this.setState({
-                input: hours
-            });
-            console.dir(start.format('YYYY-MM-DD HH:mm'));
-            console.dir(end.format('YYYY-MM-DD HH:mm'));
-        }else{
-            this.setState({
-                input: "Wrong Input"
-            });
-        }
+        var result = this.calculateHours(input);
+        this.setState({
+            input: math.eval(result)
+        });
     };
 
   render() {
@@ -60,25 +69,33 @@ class App extends Component {
                   <TimeButton handleClick={this.addToInput}>7</TimeButton>
                   <TimeButton handleClick={this.addToInput}>8</TimeButton>
                   <TimeButton handleClick={this.addToInput}>9</TimeButton>
-                  <TimeButton handleClick={this.addToInput}> AM </TimeButton>
+                  <TimeButton type="time-symbol" handleClick={this.addToInput}> AM </TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>+</TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>/</TimeButton>
               </div>
               <div className="row">
                   <TimeButton handleClick={this.addToInput}>4</TimeButton>
                   <TimeButton handleClick={this.addToInput}>5</TimeButton>
                   <TimeButton handleClick={this.addToInput}>6</TimeButton>
-                  <TimeButton handleClick={this.addToInput}> PM </TimeButton>
+                  <TimeButton type="time-symbol" handleClick={this.addToInput}> PM </TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>-</TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>(</TimeButton>
               </div>
               <div className="row">
                   <TimeButton handleClick={this.addToInput}>1</TimeButton>
                   <TimeButton handleClick={this.addToInput}>2</TimeButton>
                   <TimeButton handleClick={this.addToInput}>3</TimeButton>
-                  <TimeButton handleClick={this.addToInput}> TO </TimeButton>
+                  <TimeButton type="time-symbol" handleClick={this.addToInput}> TO </TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>*</TimeButton>
+                  <TimeButton type="operator" handleClick={this.addToInput}>)</TimeButton>
               </div>
               <div className="row">
-                  <TimeButton handleClick={this.addToInput}>:</TimeButton>
+                  <TimeButton type="time-symbol" handleClick={this.addToInput}>:</TimeButton>
                   <TimeButton handleClick={this.addToInput}>0</TimeButton>
+                  <TimeButton handleClick={this.addToInput}>00</TimeButton>
                   <TimeButton handleClick={() => this.handleEquals()}>=</TimeButton>
                   <ClearButton handleClear={() => this.setState({input: ""})}>C</ClearButton>
+                  <ClearButton handleClear={() => this.setState({input: this.state.input.slice(0,-1)})}>{`<--`}</ClearButton>
               </div>
           </div>
       </div>
--- src/components/TimeButton.js
+++ src/components/TimeButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './TimeButton.css';
 
 export const TimeButton = props => (
-    <div className={`time-button-wrapper`}
+    <div className={`time-button-wrapper ${props.type ? props.type : ""}`}
          onClick={() => props.handleClick(props.children)}
     >
         {props.children}
